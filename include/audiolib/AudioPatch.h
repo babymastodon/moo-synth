72a50ff52b5e4b1a0d195fcd420e11caf2647614
Zack Drach <hogbait@gmail.com>
2013-03-03 02:26:45 -0500

    spec-ed out the new AudioPatch API/graph

--- include/audiolib/AudioPatch.h
+++ include/audiolib/AudioPatch.h
@@ -27,74 +27,43 @@ namespace audiolib{
    * etc.)
    *
    * An audio patch can have 4 types of IO ports:
-   *  AudioSource
+   *  AudioOutput
    *      Conceptually, it is an output buffer. An AudioInput
    *      can connect to it and pull a single batch of frame
-   *      data. Internally, an AudioSource may or may not be
-   *      a buffer -- it may compute the batch on demand (in
-   *      the case of a synthesizer/instrument), or might pull
-   *      it from an AudioSource higher (closer to the source)
-   *      in the tree.
+   *      data.
    *  AudioInput
-   *      Connects to the AudioSource of another node, and
+   *      Connects to the AudioOutput of another node, and
    *      pulls new data when required. For example, a node
    *      might wrap around a DAC (sound card) thread. When
    *      the sound card's buffer needs more data, the node
    *      uses the "fetchAudioInput()" function to pull new
-   *      frame data from the connected AudioSource.
+   *      frame data from the connected AudioOutput.
    *
    * Basically, there are two types of connections:
-   *  AudioSource -> AudioInput
-   *
-   * In each case, there is a passive end (Source/Sink),
-   *      and an active end (Input/Output). At a more fundemental
-   *      level, the passive end can be thought of as a
-   *      function object -- an AudioSource
-   *      is a function that returns frame data, whereas
-   *      an AudioSink is a function that takes frame
-   *      data as an argument. The active end is a pointer
-   *      to these functions. Suppose that an AudioInput I
-   *      of a patch P is connected to the AudioSource S of
-   *      another patch. In other words, I points to S. When
-   *      the patch P needs more frame data, it uses I to
-   *      find S, then directly calls the function S to
-   *      get the new frame data.
+   *  AudioOutput -> AudioInput
    *
    * Invariants
    *  Both ends of a connection must be have the same
    *      AudioSettings.
-   *  There is a one-to-one relationship between all
-   *      connections
    *
-   * Inputs/Outputs/Sources/Sinks are 0-indexed
+   * Inputs/Outputs are 0-indexed
    *
    * Subclasses should implement the following functions:
-   *    Both constructors
-   *    sourceAudioFrame (if applicable)
-   *    sinkAudioFrame (if applicable)
+   *    constructor
    *    numAudioInputs (if applicable)
    *    numAudioOutputs (if applicable)
-   *    numAudioSources (if applicable)
-   *    numAudioSinks (if applicable)
    *
    * When two audio patches are connected, their sample rates
    *  and block size are expected to match. Additionally, all
-   *  inputs of an AudioPatch shold be connected to a Source, and
-   *  all outputs should be connected to a Sink. These invariants
-   *  are checked by calling the validate function.
+   *  inputs of an AudioPatch shold be connected to an Output.
+   *  These invariants are checked by calling the validate function.
    *
    * */
   class AudioPatch : public Patch{
     public:
 
-      /**
-       * These constructors don't do much. Subclasses
-       * are expected to implement their own constructors
-       * that spec out the input/outut/source/sink
-       * by using the "addXXX()" function.
-       */
       AudioPatch(const std::string & s, unsigned int sample_rate = 44100, unsigned int block_size = 64)
-        : Patch(s), sample_rate_(sample_rate), block_size_(block_size){}
+        : Patch(s), sample_rate_(sample_rate), block_size_(block_size), last_compute_cycle_(0){}
       /* Virtual destructor to stop memory leaks */
       virtual ~AudioPatch() {}
 
@@ -102,9 +71,8 @@ namespace audiolib{
        * function connectAudioInput
        *
        * Connects the nth audio input of "this" to the mth audio
-       * source of "other". The "AudioSettings" of the input and
-       * the source must be equal. Additionally, n and m must be
-       * valid (must refer to existing inputs/sources). Eg,
+       * output of "other". n and m must be
+       * valid (must refer to existing inputs/outputs). Eg,
        * n < this.numAudioInputs() and m < other.numaAudioOutputs().
        * If an invarient is not met, then a runtime error is thrown.
        */
@@ -117,16 +85,25 @@ namespace audiolib{
        */
       void disconnectAudioInput(int n);
 
+      /**
+       * function addAudioDependency
+       *
+       * Build a dependency graph between nodes. When
+       * the compute() function is called on a particular
+       * node, all of its parents are executed, and the
+       * results are stored in the output buffers.
+       */
+
       virtual int numAudioInputs(){return 0;}
-      virtual int numAudioSources(){return 0;}
+      virtual int numAudioOutputs(){return 0;}
 
       /**
        * function validate()
        *
        * ensures that:
-       *    all connected sources have the same sample rate
+       *    all connected outputs have the same sample rate
        *    all of the inputs on the current device
-       *      are connected to a source
+       *      are connected to an output
        *
        * raises a RuntimeError with an error message
        *    if a check fails.
@@ -138,13 +115,23 @@ namespace audiolib{
        * function fetchAudioInput(n)
        *
        * subclasses can use this function to read a new
-       * set of frames from the AudioSource that is connected
+       * set of frames from the AudioOutput that is connected
        * to the nth AudioInput of this object. n is assumed
        * to be an index for an input. Otherwise the
        * behavior is undefined
        */
       const Iframes & fetchAudioInput(int n);
 
+      /**
+       * function setAudioOutput(n, frames)
+       *
+       * subclasses can use this function to set
+       * the nth audio output buffer equal to the given
+       * frames. The frames are captured by pointer,
+       * so don't delete/modify the iframes until the
+       * next cycle.
+       */
+      void setAudioOutput(int n, const Iframes * frames);
 
       /**
        * function fetchSampleRate(n)
@@ -160,33 +147,32 @@ namespace audiolib{
       unsigned int getBlockSize() {return block_size_;}
       void setBlockSize(unsigned int n) {block_size_ = n;}
 
+      /**
+       * recomputes all of the parents of this node (starts
+       * a new cycle). Should only be used on the base
+       * of the dependency graph.
+       */
+      void recomputeParents();
+
     private:
       int sample_rate_;
       int block_size_;
 
+      int last_compute_cycle_;
+
       /**
        * keeps track of the mapping between the local
-       * Input/Output ports and the external Sources
-       * and Sinks belonging to other patches. The
-       * nth index of the external_audio_sources_
-       * vector corresponds to the AudioSource that
+       * Input input ports and the output ports
+       * belonging to other patches. The
+       * nth index of the external_audio_outputs_
+       * vector corresponds to the AudioOutput that
        * is connected to the nth AudioInput on this
        * object.
        *
        * if the pointer is null, then the port is not
        * connected
        */
-      std::unordered_map<int,AudioPortPair> external_audio_sources_;
-
-      /**
-       * when something connects to a Source or a Sink,
-       * it locks that resource, enforcing the one-to-
-       * one relationship between ports
-       *
-       * if the pointer is null, then the port is not
-       * connected
-       */
-      std::set<int> audio_source_locks_;
+      std::unordered_map<int,AudioPortPair> external_audio_outputs_;
 
       /**
        * helper functions for printing error messages
@@ -197,29 +183,24 @@ namespace audiolib{
 
 
       /**
-       * internal functions to lock and unlock the
-       * Source and Sink ports
+       * function compute(cycle)
        *
-       * lockXXX(N) tries to lock the specific port
-       * belonging to this. (by flipping the corresponding
-       * vector entry to true). If it fails, it returns false
-       *
-       * n is assumed to be a valid index
+       * If this node has not yet been computed in the
+       * current cycle, then call compute(cycle) on all
+       * of the parent dependencies, and call internalCompute()
        */
-      bool lockAudioSource(int n);
-      void unlockAudioSource(int n);
-  
+      void compute(int id);
+
       /**
-       * To be implemented by sub-classes
+       * to be implemented by subclass
        *
-       * sourceAudioFrame(int n)
-       * is supposed to return a Iframes object conforming
-       * to the settings described inputSettings(n). "N" is
-       * assumed to be an index of a valid source.
-       *
-       * default implementation raises runtime errors
+       * This gets executed after all of the current patch's
+       * dependencies are executed. The subclass is expected
+       * to use the "fetchAudioInput(n)" and "setAudioOutput(n, frames)"
+       * to fetch and save the frame data.
        */
-      const Iframes & sourceAudioFrames(int n);
+      void internalCompute();
+
   };
 
 }
--- src/audiolib/AudioPatch.cpp
+++ src/audiolib/AudioPatch.cpp
@@ -10,28 +10,28 @@ namespace audiolib{
   void AudioPatch::connectAudioInput(int n, AudioPatch & other, int m){
     if (n >= numAudioInputs())
       throw_audio_port_out_of_range("AudioInput",n);
-    if (m >= other.numAudioSources())
-      other.throw_audio_port_out_of_range("AudioSource",m);
-    if (external_audio_sources_.count(n) != 0)
+    if (m >= other.numAudioOutputs())
+      other.throw_audio_port_out_of_range("AudioOutput",m);
+    if (external_audio_outputs_.count(n) != 0)
       throw_audio_port_occupied("AudioInput",n);
-    if (not other.lockAudioSource(m))
-      other.throw_audio_port_occupied("AudioSource",m);
+    if (not other.lockAudioOutput(m))
+      other.throw_audio_port_occupied("AudioOutput",m);
     AudioPortPair pair(other, m);
-    external_audio_sources_.emplace(n,pair);
+    external_audio_outputs_.emplace(n,pair);
   }
 
   void AudioPatch::disconnectAudioInput(int n){
-    if (external_audio_sources_.count(n) == 1){
-      AudioPortPair& p = external_audio_sources_.at(n);
-      p.patch.unlockAudioSource(p.port);
-      external_audio_sources_.erase(n);
+    if (external_audio_outputs_.count(n) == 1){
+      AudioPortPair& p = external_audio_outputs_.at(n);
+      p.patch.unlockAudioOutput(p.port);
+      external_audio_outputs_.erase(n);
     }
   }
 
 
   const Iframes & AudioPatch::fetchAudioInput(int n){
-    AudioPortPair& p = external_audio_sources_.at(n);
-    return p.patch.sourceAudioFrames(p.port);
+    AudioPortPair& p = external_audio_outputs_.at(n);
+    return p.patch.outputAudioFrames(p.port);
   }
 
 
@@ -49,23 +49,9 @@ namespace audiolib{
 
 
 
-  bool AudioPatch::lockAudioSource(int n){
-    if (audio_source_locks_.count(n) == 0){
-      audio_source_locks_.insert(n);
-      return true;
-    }
-    return false;
-  }
-  void AudioPatch::unlockAudioSource(int n){
-    if (audio_source_locks_.count(n) != 0)
-      audio_source_locks_.erase(n);
-  }
-
-
-
-  const Iframes & AudioPatch::sourceAudioFrames(int n){
+  const Iframes & AudioPatch::outputAudioFrames(int n){
     std::stringstream ss;
-    ss << "NotImplemented: sourceAudioFrame(...) is not implemented for AudioPatch ";
+    ss << "NotImplemented: outputAudioFrame(...) is not implemented for AudioPatch ";
     ss << this->getName();
     throw std::runtime_error(ss.str());
   }
@@ -74,16 +60,16 @@ namespace audiolib{
   void AudioPatch::validate(){
     /* ensure that all AudioInputs are connected to something */
     for (int i=0; i<numAudioInputs(); i++){
-      if(external_audio_sources_.count(i) != 1){
+      if(external_audio_outputs_.count(i) != 1){
         std::stringstream ss;
         ss << "AudioInput " << i << " on AudioPatch " << getName();
-        ss << " needs to be connected to an AudioSource";
+        ss << " needs to be connected to an AudioOutput";
         throw std::runtime_error(ss.str());
       }
     }
-    /* ensure that all connected AudioSources have a matching
+    /* ensure that all connected AudioOutputs have a matching
      * sample rate and block size */
-    for (auto pair: external_audio_sources_){
+    for (auto pair: external_audio_outputs_){
       if (pair.second.patch.getSampleRate() != getSampleRate()){
         std::stringstream ss;
         ss << "Sample Rate mismatch between two connected AudioPatches: ";
